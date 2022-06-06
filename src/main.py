cc30191a3c2c959c119d9487e63e35f28fb70311
Joao Andrade <up201905589@fe.up.pt>
2022-06-06 22:18:40 +0100

    fix

--- src/main.py
+++ src/main.py
@@ -1,17 +1,28 @@
 import pandas as pd
 import numpy as nm
+import six
+import sys
+sys.modules['sklearn.externals.six'] = six
+import warnings
+warnings.filterwarnings('always')  # "error", "ignore", "always", "default", "module" or "once"
+warnings.filterwarnings('ignore')
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.svm import SVC
+
 from imblearn.over_sampling import SMOTE
 from imblearn.under_sampling import RandomUnderSampler
+
 from sklearn.neighbors import KNeighborsClassifier
 from collections import Counter
+
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 
 # Using pandas to read the dataset file in order to visualize it
+from sklearn.tree import DecisionTreeClassifier
+
 dataset = pd.read_csv("C:/Users/ASUS/Desktop/FEUP/3ano/2Semestre/IART/PROJ-IART2/src/data.csv")
 
 # Creating the correlation matrix in order to eliminate any variables with a correlation higher than 0.95 since they
@@ -30,23 +41,52 @@ labels = dataset.iloc[:, -1].values
 x_train, x_test, y_train, y_test = train_test_split(inputs, labels, test_size=0.25, random_state=0)
 
 
-ovsp = SMOTE(random_state=1)
+oversample = SMOTE(random_state=1)
 unsp = RandomUnderSampler(random_state=1)
 
-ovsp_inputs, ovsp_labels = ovsp.fitresample(inputs, labels)
-print(Counter(ovsp_labels))
+oinputs, olabels = oversample.fit_resample(inputs, labels)
+print(Counter(olabels))
 
-unsp_inputs, unsp_labels = unsp.fitresample(inputs, labels)
+unsp_inputs, unsp_labels = unsp.fit_resample(inputs, labels)
 print(Counter(unsp_labels))
 
 
+# scaler = StandardScaler()
+# scaler.fit(x_train)
+#
+# x_train = scaler.fit_transform(x_train)
+# x_test = scaler.fit_transform(x_test)
 
-st_x = StandardScaler()
-x_train = st_x.fit_transform(x_train)
-x_test = st_x.transform(x_test)
+# scaler.fit(os_inputs)
+# os_inputs = scaler.fit_transform(os_inputs)
+#
+# scaler.fit(us_inputs)
+# us_inputs = scaler.fit_transform(us_inputs)
 
+# st_x = StandardScaler()
+# x_train = st_x.fit_transform(x_train)
+# x_test = st_x.transform(x_test)
 
-# Decision Trees Algorithm
+# svm_classifier = SVC(random_state=0)
+#
+#
+# svm_tuned_parameters = [{'kernel': ['rbf', 'linear','poly','sigmoid'],
+#                        'gamma': ['auto','scale', 1e-3, 1e-4],
+#                        'C': [0.01, 0.1, 1, 10, 100],
+#                        'tol':[1e-4, 1e-3]}]
+#
+#
+# svm_grid_search = GridSearchCV(svm_classifier,
+#                             param_grid=svm_tuned_parameters,
+#                             scoring='precision_weighted',
+#                             n_jobs=-1,
+#                             cv=10)
+#
+#
+# svm_grid_search.fit(x_train, y_train)
+# print('Best score: {}'.format(svm_grid_search.best_score_))
+# print('Best parameters: {}'.format(svm_grid_search.best_params_))
+# # Decision Trees Algorithm
 #
 # classifier = DecisionTreeClassifier(criterion='entropy', random_state=0)
 # classifier.fit(x_train, y_train)
@@ -54,10 +94,10 @@ x_test = st_x.transform(x_test)
 # y_pred = classifier.predict(x_test)
 #
 # cm = confusion_matrix(y_test, y_pred)
+
+# Calculating the accurracy of the Decision Tree algorithm
 #
-# # Calculating the accurracy of the Decision Tree algorithm
-#
-# print('\nAccuracy: {:.2f}\n'.format(accuracy_score(y_test, y_pred)))
+# print('\nAccuracy: {:}\n'.format(accuracy_score(y_test, y_pred)))
 #
 # # It is calculated by considering the total TP, total FP and total FN of the confusion matrix. It does not consider each class individually, It calculates the metrics globally
 #
@@ -126,15 +166,6 @@ x_test = st_x.transform(x_test)
 # cm = confusion_matrix(y_test, y_pred)
 
 # MLP - Multilayer Perceptron
-
-classifier = MLPClassifier(random_state=1, early_stopping=False)
-classifier.fit(x_train, y_train)
-
-y_pred = classifier.predict(x_test)
-print("MLP - Multilayer Perceptron accuracy: {:}".format(accuracy_score(y_test, y_pred)))
-
-
-
 
 # print(y_test)
 # print(y_pred)
